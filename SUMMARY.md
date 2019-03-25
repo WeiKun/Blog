271b33383a1f376203693ff953eb9ba716d785cb
WeiKun <weikun_ict@163.com>
2019-03-25 14:32:07 +0800

    add

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,4 +1,35 @@
-# Table of contents
+# Summary
 
-* [Initial page](README.md)
+* [Introduction](README.md)
+
+### Unix
+
+* [Try ksm](unix/2019_03_10_try_ksm.md)
+
+
+### Python
+
+* [Mypy introduction](python/2019_01_24_mypy_introduction.md)
+* [List sort optimize](python/2019_03_03_list_sort_optimize.md)
+
+
+### Gamesvr-dev
+
+* [Pybot xproto2py](gamesvr-dev/2019_01_13_pybot_xproto2py.md)
+* [Share data](gamesvr-dev/2019_01_05_share_data.md)
+* [Shareres based on share memory](gamesvr-dev/2019_03_22_shareres_based_on_share_memory.md)
+
+
+### Cpp
+
+* [Duffs device](cpp/2019_02_14_duffs_device.md)
+* [Singleton template](cpp/2019_03_18_singleton_template.md)
+* [Bug in cout](cpp/2019_01_20_bug_in_cout.md)
+* [Initialization and constructor](cpp/2019_01_13_initialization_and_constructor.md)
+
+
+### Linker-and-loader
+
+* [Training of programmer static link](linker-and-loader/2019_02_08_training_of_programmer_static_link.md)
+* [Training of programmer load](linker-and-loader/2019_02_23_training_of_programmer_load.md)
 
---
+++ generate_summary.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# Author: weikun
+# Created Time: Mon 25 Mar 2019 10:17:01 AM CST
+
+f = open('SUMMARY.md', 'r+')
+infos = f.readlines()
+f.close()
+
+dirs=[]
+dirHash = {}
+curDir = None
+curDirName = None
+
+for info in infos[4:]:
+    info = info.strip('\n')
+    if info.startswith('###'):
+        curDir = []
+        curDirName = info[info.find(' ') + 1:]
+        dirs.append((curDirName, curDir))
+
+    elif info.startswith('*'):
+        name = info[info.find('[') + 1:info.find(']')]
+        filePath = info[info.find('(') + 1:info.find(')')]
+        curDir.append((name, filePath))
+        if dirHash.get(filePath.split('/')[0], None) == None:
+            dirHash[filePath.split('/')[0]] = (curDirName, curDir)
+
+
+import os
+
+def rebuildName(fileName):
+    fileName = fileName[11:-3] #len(xxxx_xx_xx) = 11 len(.md) = 3
+    fileName = fileName.replace('_', ' ')
+    fileName = fileName.capitalize()
+    return fileName
+
+def checkRepeat(fileName, curDir):
+    for name, fName in curDir:
+        if fName == fileName:
+            return False
+    else:
+        return True
+
+for maindir, subdir, file_name_list in os.walk('.'):
+    if not file_name_list:
+        continue
+    
+    if '_book' in maindir:
+        continue
+
+    if not maindir.startswith('./'):
+        continue
+
+    for file_name in file_name_list:
+        if not file_name.endswith('.md'):
+            continue
+
+        pathDirName = maindir[len('./'):]
+        fileName = file_name
+        if pathDirName in dirHash:
+            curDirName, curDir = dirHash[pathDirName]
+            if checkRepeat('%s/%s' % (pathDirName, fileName), curDir):
+                name = rebuildName(fileName)
+                curDir.append((name, '%s/%s' % (pathDirName, fileName)))
+        else:
+            curDirName = pathDirName.capitalize()
+            curDir = []
+            dirHash[pathDirName] = (curDirName, curDir)
+            dirs.append((curDirName, curDir))
+            name = rebuildName(fileName)
+            curDir.append((name, '%s/%s' % (pathDirName, fileName)))
+
+writeinfos = []
+writeinfos.append('# Summary\n')
+writeinfos.append('* [Introduction](README.md)\n')
+for name, curDir in dirs:
+    writeinfos.append('### %s\n' % (name))
+    for d in curDir:
+        writeinfos.append('* [%s](%s)' % (d[0], d[1]))
+    writeinfos.append('\n')
+
+f = open('SUMMARY.md', 'w+')
+f.writelines('\n'.join(writeinfos))
+f.close()
